cc5b393583a53bfb4c97afc078cba1b997a93f9a
오세진 <osejin@osejins-MacBook-Pro.local>
2023-04-30 23:15:41 +0900

    돔을 이용하여 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,15 @@
-console.log("hello");
-console.log("bey");
+"use strict";
+
+const id = document.querySelector("#id"),
+    pwd = document.querySelector("#pwd"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        pwd : pwd.value,
+    };
+    console.log(req);
+}
